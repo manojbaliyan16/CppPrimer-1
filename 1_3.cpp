70e0e48d4a4953395ffd6fbcff44b3fa60c77ebc
manoj <manoj.kumar@kaiostech.com>
2018-02-11 23:20:27 +0530

    basic program

---
+++ 1_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void fun1(){
+
+	cout << "Three blind mice" <<endl;
+}
+void fun2(){
+	cout << "See How They Run"<<endl;
+}
+int main(){
+	fun1();
+	fun1();
+	fun2();
+	fun2();
+		return 0;
+}
+
+
+
---
+++ 1_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+	int age;
+	cout << "Enter Your Age:" ;
+	cin >> age ;
+	cout << "Your age in months is " << age*12 <<endl;
+	return 0;
+}
